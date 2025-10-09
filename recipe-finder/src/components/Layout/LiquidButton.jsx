232c7996398056bc3b98a395530220ade1a6f14a
brianstatesthat <mbspeelman@gmail.com>
2025-10-10 00:58:54 +0200

    liquid glass implemented

--- recipe-finder/src/components/Layout/LiquidButton.jsx
+++ recipe-finder/src/components/Layout/LiquidButton.jsx
@@ -7,7 +7,7 @@ function LiquidButton() {
       displacementScale={100}
       blurAmount={0.01}
      cornerRadius={100}
-      padding="9px 21px"
+      padding="12px 21px"
       className='select-none'
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
     <span className="text-white text-4xl font-extrabold w-24 h-24 rounded-full select-none">↑</span>
