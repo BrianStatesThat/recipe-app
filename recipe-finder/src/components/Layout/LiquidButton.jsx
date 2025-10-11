45cf085780e47f45ea6345a87f4b1212343c819f
brianstatesthat <mbspeelman@gmail.com>
2025-10-11 17:00:24 +0200

    UI Enhancement

--- recipe-finder/src/components/Layout/LiquidButton.jsx
+++ recipe-finder/src/components/Layout/LiquidButton.jsx
@@ -7,7 +7,7 @@ function LiquidButton() {
       displacementScale={100}
       blurAmount={0.01}
       cornerRadius={100}
-      padding="10px 20px"
+      padding="15px 20px"
       className='select-none rounded-full'
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
     <span className="text-white text-4xl font-extrabold w-24 h-24 rounded-full select-none">↑</span>
